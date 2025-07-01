02f4c7f59afe8f549699af2bf5918d2c185c8108
Johnathan Martins <johnathanfoxandrade@gmail.com>
2025-06-30 21:22:19 -0300

    Atualizações commits

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Bloco de código para testar a classe Nokia
+
 Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "123456789", modelo: "Nokia g23", imei: "11123123" , memoria: 64);
 nokia.Ligar();
 nokia.InstalarAplicativo("Whatsapp");
 
 Console.WriteLine("\n-------------------------\n");
+
+// Bloco de código para testar a classe Iphone
 
 Console.WriteLine("Smartphone iPhone:");
 Smartphone iphone = new Iphone(numero: "98324025", modelo: "Iphone 15 pro", imei: "16534878" , memoria: 128);
